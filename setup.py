aeac5ae2bcf06f033c869f086b5ef85d8ceef732
yakolle <yakolle@aliyun.com>
2018-10-07 09:20:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='ce-tune',
+setup(name='ce_tune',
       version='0.1',
       description="Tune the ml model's superparameters",
-      url='http://github.com/yakolle/ce-tune',
+      url='http://github.com/yakolle/ce_tune',
       author='Yakolle Zhang',
       license='MIT',
-      packages=['ce-tune'],
+      packages=['ce_tune'],
       zip_safe=False)      
